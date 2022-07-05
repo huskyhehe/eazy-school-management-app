8c33af349747b70e6a746fa35930f037c4851fb8
huskyhehe <zhou.he1@northeastern.edu>
2022-07-05 01:58:19 -0700

    Add Initializr

---
+++ src/test/java/com/huksyhehe/dayzschool/DayzschoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huksyhehe.dayzschool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DayzschoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
