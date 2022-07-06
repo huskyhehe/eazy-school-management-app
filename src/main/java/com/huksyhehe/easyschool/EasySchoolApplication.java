68b6b7756441684c15b338bb80b0e20eaba47f18
huskyhehe <zhou.he1@northeastern.edu>
2022-07-06 13:56:58 -0700

    Rename Application.java

--- src/main/java/com/huksyhehe/easyschool/EasySchoolApplication.java
+++ src/main/java/com/huksyhehe/easyschool/EasySchoolApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EasyschoolApplication {
+public class EasySchoolApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EasyschoolApplication.class, args);
+		SpringApplication.run(EasySchoolApplication.class, args);
 	}
 
 }
