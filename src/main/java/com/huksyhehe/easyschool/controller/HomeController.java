fe87cfefbe7927914d0ade5a3923fcd4d026167a
huskyhehe <zhou.he1@northeastern.edu>
2022-07-06 16:18:31 -0700

    Update HomeController.java

--- src/main/java/com/huksyhehe/easyschool/controller/HomeController.java
+++ src/main/java/com/huksyhehe/easyschool/controller/HomeController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HomeController {
 
     @RequestMapping(value = {"", "/", "/home"})
-    public String displayHomePage(Model model) {
+    public String displayHomePage() {
         return "home.html";
     }
 
