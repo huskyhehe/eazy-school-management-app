37c73532b1056177aab539938cc57279f2ef73c6
huskyhehe <zhou.he1@northeastern.edu>
2022-07-09 17:00:39 -0700

    Add Contact Model, Controller, Service

--- src/main/java/com/huksyhehe/easyschool/controller/ContactController.java
+++ src/main/java/com/huksyhehe/easyschool/controller/ContactController.java
@@ -1,2 +1,31 @@
-package com.huksyhehe.easyschool.controller;public class ContactController {
+package com.huksyhehe.easyschool.controller;
+
+import com.huksyhehe.easyschool.model.Contact;
+import com.huksyhehe.easyschool.service.ContactService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
+
+public class ContactController {
+
+    private final ContactService contactService;
+
+    @Autowired
+    public ContactController(ContactService contactService) {
+        this.contactService = contactService;
+    }
+
+    @RequestMapping("/contact")
+    public String displayContactPage() {
+        return "contact.html";
+    }
+
+    @RequestMapping(value = "/saveMsg",method = POST)
+    public ModelAndView saveMessage(Contact contact){
+        contactService.saveMessageDetails(contact);
+        return new ModelAndView("redirect:/contact");
+    }
+
 }
--- src/main/java/com/huksyhehe/easyschool/model/Contact.java
+++ src/main/java/com/huksyhehe/easyschool/model/Contact.java
@@ -1,2 +1,19 @@
-package com.huksyhehe.easyschool.model;public class Contact {
+package com.huksyhehe.easyschool.model;
+
+
+import lombok.Data;
+
+/*
+@Data annotation is provided by Lombok library which generates getter, setter,
+equals(), hashCode(), toString() methods & Constructor at compile time.
+This makes our code short and clean.
+* */
+@Data
+public class Contact {
+
+    private String name;
+    private String mobileNum;
+    private String email;
+    private String subject;
+    private String message;
 }
