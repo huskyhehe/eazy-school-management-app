0b0c8583f5bc0687c38a00124b0caad4dbd2087b
huskyhehe <zhou.he1@northeastern.edu>
2022-07-10 22:59:30 -0700

    Add validation

--- src/main/java/com/huksyhehe/easyschool/model/Contact.java
+++ src/main/java/com/huksyhehe/easyschool/model/Contact.java
@@ -3,6 +3,11 @@ package com.huksyhehe.easyschool.model;
 
 import lombok.Data;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
 /*
 @Data annotation is provided by Lombok library which generates getter, setter,
 equals(), hashCode(), toString() methods & Constructor at compile time.
@@ -11,9 +16,24 @@ This makes our code short and clean.
 @Data
 public class Contact {
 
+    @NotBlank(message = "Name must not be blank")
+    @Size(min = 3, message = "Name must not be at least 3 characters long")
     private String name;
+
+    @NotBlank(message="Mobile number must not be blank")
+    @Pattern(regexp="(^$|[0-9]{10})",message = "Mobile number must be 10 digits")
     private String mobileNum;
+
+    @NotBlank(message="Email must not be blank")
+    @Email(message = "Please provide a valid email address" )
     private String email;
+
+    @NotBlank(message="Subject must not be blank")
+    @Size(min=5, message="Subject must be at least 5 characters long")
     private String subject;
+
+    @NotBlank(message="Message must not be blank")
+    @Size(min=10, message="Message must be at least 10 characters long")
     private String message;
+
 }
