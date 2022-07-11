d7530cd150834f25184eba8a2141d104e1da821d
huskyhehe <zhou.he1@northeastern.edu>
2022-07-10 22:59:44 -0700

    Add validation

--- src/main/java/com/huksyhehe/easyschool/controller/ContactController.java
+++ src/main/java/com/huksyhehe/easyschool/controller/ContactController.java
@@ -2,12 +2,20 @@ package com.huksyhehe.easyschool.controller;
 
 import com.huksyhehe.easyschool.model.Contact;
 import com.huksyhehe.easyschool.service.ContactService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
+
+import javax.validation.Valid;
 
 import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
+@Slf4j
+@Controller
 public class ContactController {
 
     private final ContactService contactService;
@@ -18,14 +26,20 @@ public class ContactController {
     }
 
     @RequestMapping("/contact")
-    public String displayContactPage() {
+    public String displayContactPage(Model model) {
+        model.addAttribute("contact", new Contact());
         return "contact.html";
     }
 
     @RequestMapping(value = "/saveMsg",method = POST)
-    public ModelAndView saveMessage(Contact contact){
+    public String saveMessage(@Valid @ModelAttribute("contact") Contact contact, Errors errors){
+
+        if(errors.hasErrors()){
+            log.error("Contact form validation failed due to : " + errors.toString());
+            return "contact.html";
+        }
         contactService.saveMessageDetails(contact);
-        return new ModelAndView("redirect:/contact");
+        return "redirect:/contact";
     }
 
 }
