105dcc5f30d9c07b0ad2b7d3f80788f4564badef
huskyhehe <zhou.he1@northeastern.edu>
2022-07-11 11:37:50 -0700

    Custom Security Configurations for public pages & CSRF Disable

--- src/main/java/com/huksyhehe/easyschool/config/ProjectSecurityConfig.java
+++ src/main/java/com/huksyhehe/easyschool/config/ProjectSecurityConfig.java
@@ -10,19 +10,14 @@ public class ProjectSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
-        // Permit All Requests inside the Web Application
-        http.authorizeRequests().
-                anyRequest().permitAll().
-                and().formLogin()
-                .and().httpBasic();
+        http.authorizeHttpRequests()
+                .mvcMatchers("/home").permitAll()
+                .mvcMatchers("holidays/**").permitAll()
+                .mvcMatchers("/saveMsg").permitAll()
+                .mvcMatchers("/courses").permitAll()
+                .mvcMatchers("/about").permitAll()
+                .and().formLogin().and().httpBasic();
 
-        // Deny All Requests inside the Web Application
-        /**
-        http.authorizeRequests().
-                anyRequest().denyAll().
-                and().formLogin()
-                .and().httpBasic();
-        */
     }
 
 }
