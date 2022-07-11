8157e31a100e81db661ec675151c086bbd2d47bd
huskyhehe <zhou.he1@northeastern.edu>
2022-07-11 11:33:26 -0700

    Add SecurityConfig

---
+++ src/main/java/com/huksyhehe/easyschool/config/ProjectSecurityConfig.java
@@ -0,0 +1,28 @@
+package com.huksyhehe.easyschool.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class ProjectSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        // Permit All Requests inside the Web Application
+        http.authorizeRequests().
+                anyRequest().permitAll().
+                and().formLogin()
+                .and().httpBasic();
+
+        // Deny All Requests inside the Web Application
+        /**
+        http.authorizeRequests().
+                anyRequest().denyAll().
+                and().formLogin()
+                .and().httpBasic();
+        */
+    }
+
+}
