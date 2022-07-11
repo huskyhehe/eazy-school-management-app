da491bc1f9e5678799577b8b7a0605b6e03a1b3d
huskyhehe <zhou.he1@northeastern.edu>
2022-07-11 11:51:51 -0700

    Add users using inMemoryAuthentication()

--- src/main/java/com/huksyhehe/easyschool/config/ProjectSecurityConfig.java
+++ src/main/java/com/huksyhehe/easyschool/config/ProjectSecurityConfig.java
@@ -1,8 +1,11 @@
 package com.huksyhehe.easyschool.config;
 
+import org.springframework.cglib.proxy.NoOp;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 
 @Configuration
 public class ProjectSecurityConfig extends WebSecurityConfigurerAdapter {
@@ -18,7 +21,15 @@ public class ProjectSecurityConfig extends WebSecurityConfigurerAdapter {
                 .mvcMatchers("/courses").permitAll()
                 .mvcMatchers("/about").permitAll()
                 .and().formLogin().and().httpBasic();
+    }
 
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user").password("12345").roles("USER")
+                .and()
+                .withUser("admin").password("admin").roles("USER", "ADMIN")
+                .and().passwordEncoder(NoOpPasswordEncoder.getInstance());
     }
 
 }
