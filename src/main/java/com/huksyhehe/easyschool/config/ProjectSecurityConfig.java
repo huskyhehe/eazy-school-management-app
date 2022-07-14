71f7236d167f4d40be3bd689afb5868f1962aa36
huskyhehe <zhou.he1@northeastern.edu>
2022-07-13 17:14:51 -0700

    Add security exception using ignoringAntMatchers

--- src/main/java/com/huksyhehe/easyschool/config/ProjectSecurityConfig.java
+++ src/main/java/com/huksyhehe/easyschool/config/ProjectSecurityConfig.java
@@ -12,9 +12,10 @@ public class ProjectSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
-        http.csrf().disable()
+        http.csrf().ignoringAntMatchers("/saveMsg").ignoringAntMatchers("/h2-console/**").and()
                 .authorizeRequests()
                 .mvcMatchers("/dashboard").authenticated()
+                .mvcMatchers("/displayMessages").hasRole("ADMIN")
                 .mvcMatchers("/home").permitAll()
                 .mvcMatchers("/holidays/**").permitAll()
                 .mvcMatchers("/contact").permitAll()
@@ -25,7 +26,10 @@ public class ProjectSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and().formLogin().loginPage("/login")
                 .defaultSuccessUrl("/dashboard").failureUrl("/login?error=true").permitAll()
                 .and().logout().logoutSuccessUrl("/login?logout=true").invalidateHttpSession(true).permitAll()
+                .and().authorizeRequests().antMatchers("/h2-console/**").permitAll()
                 .and().httpBasic();
+
+        http.headers().frameOptions().disable();
     }
 
     @Override
