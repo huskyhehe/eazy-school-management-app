a2b0d9e9cf59507e917d3cd0eae0f7aa6df51560
huskyhehe <zhou.he1@northeastern.edu>
2022-07-15 16:16:11 -0700

    Migrate from jdbc to jpa CrudRepository

--- src/main/java/com/huskyhehe/eazyschool/repository/ContactRepository.java
+++ src/main/java/com/huskyhehe/eazyschool/repository/ContactRepository.java
@@ -7,55 +7,14 @@ package com.huskyhehe.eazyschool.repository;
 * */
 
 import com.huskyhehe.eazyschool.model.Contact;
-import com.huskyhehe.eazyschool.rowmapper.ContactRowMapper;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.PreparedStatementSetter;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-import java.sql.Timestamp;
-import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
-public class ContactRepository {
+public interface ContactRepository extends CrudRepository<Contact, Integer> {
 
-    private final JdbcTemplate jdbcTemplate;
-
-    @Autowired
-    public ContactRepository(JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
-    }
-
-    public int saveContactMsg(Contact contact){
-        String sql = "insert into contact_msg (name,mobile_num,email,subject,message,status," +
-                "created_at,created_by) values (?,?,?,?,?,?,?,?)";
-        return jdbcTemplate.update(sql,contact.getName(),contact.getMobileNum(),
-                contact.getEmail(),contact.getSubject(),contact.getMessage(),
-                contact.getStatus(),contact.getCreatedAt(),contact.getCreatedBy());
-    }
-
-    public List<Contact> findMsgsWithStatus(String status) {
-        String sql = "select * from contact_msg where status = ?";
-        return jdbcTemplate.query(sql,new PreparedStatementSetter() {
-            public void setValues(PreparedStatement preparedStatement) throws SQLException {
-                preparedStatement.setString(1, status);
-            }
-        },new ContactRowMapper());
-    }
-
-    public int updateMsgStatus(int contactId, String status,String updatedBy) {
-        String sql = "update contact_msg set status = ?, updated_by = ?,updated_at =? where contact_id = ?";
-        return jdbcTemplate.update(sql,new PreparedStatementSetter() {
-            public void setValues(PreparedStatement preparedStatement) throws SQLException {
-                preparedStatement.setString(1, status);
-                preparedStatement.setString(2, updatedBy);
-                preparedStatement.setTimestamp(3, Timestamp.valueOf(LocalDateTime.now()));
-                preparedStatement.setInt(4, contactId);
-            }
-        });
-    }
+    List<Contact> findByStatus(String status);
 
 }
--- src/main/java/com/huskyhehe/eazyschool/repository/HolidayRepository.java
+++ src/main/java/com/huskyhehe/eazyschool/repository/HolidayRepository.java
@@ -1,27 +1,11 @@
 package com.huskyhehe.eazyschool.repository;
 
 import com.huskyhehe.eazyschool.model.Holiday;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.BeanPropertyRowMapper;
-import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.data.repository.CrudRepository;
+
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 
 @Repository
-public class HolidayRepository {
-
-    private final JdbcTemplate jdbcTemplate;
-
-    @Autowired
-    public HolidayRepository(JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
-    }
-
-    public List<Holiday> findAllHolidays() {
-        String sql = "SELECT * FROM holidays";
-        var rowMapper = BeanPropertyRowMapper.newInstance(Holiday.class);
-        return jdbcTemplate.query(sql, rowMapper);
-    }
-
+public interface HolidayRepository extends CrudRepository<Holiday, String> {
 }
