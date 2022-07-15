d4784ce55129768d11df48f107c16b5bbb120149
huskyhehe <zhou.he1@northeastern.edu>
2022-07-15 16:12:38 -0700

    Add @MappedSuperclass

--- src/main/java/com/huskyhehe/eazyschool/model/BaseEntity.java
+++ src/main/java/com/huskyhehe/eazyschool/model/BaseEntity.java
@@ -2,9 +2,11 @@ package com.huskyhehe.eazyschool.model;
 
 import lombok.Data;
 
+import javax.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
 
 @Data
+@MappedSuperclass
 public class BaseEntity {
     private LocalDateTime createdAt;
     private String createdBy;
