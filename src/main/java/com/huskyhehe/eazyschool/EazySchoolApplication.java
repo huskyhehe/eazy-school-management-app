9a29282fb3746c887c1e6a65cd9f73c3d5bc562b
huskyhehe <zhou.he1@northeastern.edu>
2022-07-15 16:48:48 -0700

    Add @EnableJpaRepositories and @EntityScan

--- src/main/java/com/huskyhehe/eazyschool/EazySchoolApplication.java
+++ src/main/java/com/huskyhehe/eazyschool/EazySchoolApplication.java
@@ -2,8 +2,12 @@ package com.huskyhehe.eazyschool;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories("com.huskyhehe.eazyschool.repository")
+@EntityScan("com.huskyhehe.eazyschool.model")
 public class EazySchoolApplication {
 
 	public static void main(String[] args) {
