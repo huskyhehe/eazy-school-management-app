71df1e87cf5eaf9dbe195da8c53c38e19eef74f9
huskyhehe <zhou.he1@northeastern.edu>
2022-07-15 16:24:43 -0700

    Migrate to entity class

--- src/main/java/com/huskyhehe/eazyschool/model/Holiday.java
+++ src/main/java/com/huskyhehe/eazyschool/model/Holiday.java
@@ -2,11 +2,19 @@ package com.huskyhehe.eazyschool.model;
 
 import lombok.Data;
 
+import javax.persistence.*;
+
 @Data
+@Entity
+@Table(name="holidays")
 public class Holiday extends BaseEntity {
 
+    @Id
     private String day;
+
     private String reason;
+
+    @Enumerated(EnumType.STRING)
     private Type type;
 
     public enum Type {
