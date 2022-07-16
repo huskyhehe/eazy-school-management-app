635aaed607e477ed17dcacc87dfa0586791c1371
huskyhehe <zhou.he1@northeastern.edu>
2022-07-16 15:21:59 -0700

    Add Person and PersonService

---
+++ src/main/java/com/huskyhehe/eazyschool/model/Person.java
@@ -0,0 +1,9 @@
+package com.huskyhehe.eazyschool.model;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+
+@Data
+public class Person extends BaseEntity {
+}
---
+++ src/main/java/com/huskyhehe/eazyschool/service/PersonService.java
@@ -0,0 +1,9 @@
+package com.huskyhehe.eazyschool.service;
+
+import com.huskyhehe.eazyschool.model.Person;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PersonService {
+
+}
