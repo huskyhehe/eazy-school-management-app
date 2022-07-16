835c507c487f5444d3cef49521c21f73d57d86a5
huskyhehe <zhou.he1@northeastern.edu>
2022-07-16 16:45:15 -0700

    Update PersonService

--- src/main/java/com/huskyhehe/eazyschool/service/PersonService.java
+++ src/main/java/com/huskyhehe/eazyschool/service/PersonService.java
@@ -1,12 +1,23 @@
 package com.huskyhehe.eazyschool.service;
 
 import com.huskyhehe.eazyschool.model.Person;
+import com.huskyhehe.eazyschool.repository.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PersonService {
 
-    public boolean createNewPerson(Person person) {
-        return true;
+    @Autowired
+    private PersonRepository personRepository;
+
+    public boolean createNewPerson(Person person){
+        boolean isSaved = false;
+
+        person = personRepository.save(person);
+        if (person != null && person.getPersonId() > 0) {
+            isSaved = true;
+        }
+        return isSaved;
     }
 }
