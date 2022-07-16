2c27921f9485cb33e11b4f80a4cada2ddf01a32a
huskyhehe <zhou.he1@northeastern.edu>
2022-07-16 15:21:14 -0700

    Add PublicController

--- src/main/java/com/huskyhehe/eazyschool/controller/PublicController.java
+++ src/main/java/com/huskyhehe/eazyschool/controller/PublicController.java
@@ -9,6 +9,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+
 @Slf4j
 @Controller
 @RequestMapping("public")
