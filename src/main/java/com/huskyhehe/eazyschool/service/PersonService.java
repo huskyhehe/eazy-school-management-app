922429786973559a7f80158224b2c4ab674efadc
huskyhehe <zhou.he1@northeastern.edu>
2022-07-16 20:22:23 -0700

    Add PersonService

--- src/main/java/com/huskyhehe/eazyschool/service/PersonService.java
+++ src/main/java/com/huskyhehe/eazyschool/service/PersonService.java
@@ -1,7 +1,10 @@
 package com.huskyhehe.eazyschool.service;
 
+import com.huskyhehe.eazyschool.constants.EazySchoolConstants;
 import com.huskyhehe.eazyschool.model.Person;
+import com.huskyhehe.eazyschool.model.Role;
 import com.huskyhehe.eazyschool.repository.PersonRepository;
+import com.huskyhehe.eazyschool.repository.RoleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,11 +14,15 @@ public class PersonService {
     @Autowired
     private PersonRepository personRepository;
 
+    @Autowired
+    private RoleRepository roleRepository;
+
     public boolean createNewPerson(Person person){
         boolean isSaved = false;
-
+        Role role = roleRepository.getByRoleName(EazySchoolConstants.STUDENT_ROLE);
+        person.setRoles(role);
         person = personRepository.save(person);
-        if (person != null && person.getPersonId() > 0) {
+        if (null != person && person.getPersonId() > 0) {
             isSaved = true;
         }
         return isSaved;
