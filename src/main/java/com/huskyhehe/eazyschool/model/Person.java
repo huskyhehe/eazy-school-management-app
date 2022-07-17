7e8b850573635e37b214d6ed0df114711bb20f1c
huskyhehe <zhou.he1@northeastern.edu>
2022-07-16 20:13:05 -0700

    Update Person

--- src/main/java/com/huskyhehe/eazyschool/model/Person.java
+++ src/main/java/com/huskyhehe/eazyschool/model/Person.java
@@ -1,5 +1,7 @@
 package com.huskyhehe.eazyschool.model;
 
+import com.huskyhehe.eazyschool.annotation.FieldsValueMatch;
+import com.huskyhehe.eazyschool.annotation.PasswordValidator;
 import lombok.Data;
 import org.hibernate.annotations.GenericGenerator;
 
@@ -9,35 +11,60 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
+
 @Data
 @Entity
-public class Person extends BaseEntity {
+@FieldsValueMatch.List({
+        @FieldsValueMatch(
+                field = "pwd",
+                fieldMatch = "confirmPwd",
+                message = "Passwords do not match!"
+        ),
+        @FieldsValueMatch(
+                field = "email",
+                fieldMatch = "confirmEmail",
+                message = "Email addresses do not match!"
+        )
+})
+public class Person extends BaseEntity{
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
-    @GenericGenerator(name = "native", strategy = "native")
+    @GeneratedValue(strategy= GenerationType.AUTO,generator="native")
+    @GenericGenerator(name = "native",strategy = "native")
     private int personId;
 
-    @NotBlank(message = "Name must not be blank")
-    @Size(min = 3, message = "Name must be at least 3 characters long")
+    @NotBlank(message="Name must not be blank")
+    @Size(min=3, message="Name must be at least 3 characters long")
     private String name;
 
-    @NotBlank(message = "Mobile number must not be blank")
-    @Pattern(regexp = "(^$|[0-9]{10})", message = "Mobile number must be 10 digits")
+    @NotBlank(message="Mobile number must not be blank")
+    @Pattern(regexp="(^$|[0-9]{10})",message = "Mobile number must be 10 digits")
     private String mobileNumber;
 
-    @NotBlank(message = "Email must not be blank")
-    @Email(message = "Please provide a valid email address")
+    @NotBlank(message="Email must not be blank")
+    @Email(message = "Please provide a valid email address" )
     private String email;
 
-    @NotBlank(message = "Confirm Email must not be blank")
-    @Email(message = "Please provide a valid confirm email address")
+    @NotBlank(message="Confirm Email must not be blank")
+    @Email(message = "Please provide a valid confirm email address" )
     @Transient
     private String confirmEmail;
 
-    @NotBlank(message = "Confirm Password must not be blank")
-    @Size(min = 5, message = "Confirm Password must be at least 5 characters long")
+    @NotBlank(message="Password must not be blank")
+    @Size(min=5, message="Password must be at least 5 characters long")
+    @PasswordValidator
+    private String pwd;
+
+    @NotBlank(message="Confirm Password must not be blank")
+    @Size(min=5, message="Confirm Password must be at least 5 characters long")
     @Transient
     private String confirmPwd;
 
+    @OneToOne(fetch = FetchType.EAGER,cascade = CascadeType.PERSIST, targetEntity = Roles.class)
+    @JoinColumn(name = "role_id", referencedColumnName = "roleId",nullable = false)
+    private Roles roles;
+
+    @OneToOne(fetch = FetchType.EAGER,cascade = CascadeType.ALL, targetEntity = Address.class)
+    @JoinColumn(name = "address_id", referencedColumnName = "addressId",nullable = true)
+    private Address address;
 }
