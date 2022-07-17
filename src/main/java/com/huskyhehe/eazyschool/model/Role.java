9ea0e3eb7a9ed6ac79c2c080cc8d2f46a0bf01a6
huskyhehe <zhou.he1@northeastern.edu>
2022-07-16 20:14:10 -0700

    Add Role

---
+++ src/main/java/com/huskyhehe/eazyschool/model/Role.java
@@ -0,0 +1,22 @@
+package com.huskyhehe.eazyschool.model;
+
+import lombok.Data;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Data
+@Entity
+public class Role extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO,generator="native")
+    @GenericGenerator(name = "native",strategy = "native")
+    private int roleId;
+
+    private String roleName;
+
+}
