72c1601369d72a95adc8c465aa8350db106fc0ff
huskyhehe <zhou.he1@northeastern.edu>
2022-07-17 19:23:24 -0700

    Update ZipCode as String

--- src/main/java/com/huskyhehe/eazyschool/model/Address.java
+++ src/main/java/com/huskyhehe/eazyschool/model/Address.java
@@ -36,6 +36,6 @@ public class Address extends BaseEntity{
 
     @NotBlank(message="Zip Code must not be blank")
     @Pattern(regexp="(^$|[0-9]{5})",message = "Zip Code must be 5 digits")
-    private int zipCode;
+    private String zipCode;
 
 }
