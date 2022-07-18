bd62e4494ab33dd23a4370b6fabc905f3cca408b
huskyhehe <zhou.he1@northeastern.edu>
2022-07-17 18:05:41 -0700

    Update FieldsValueMatchValidator

--- src/main/java/com/huskyhehe/eazyschool/validator/FieldsValueMatchValidator.java
+++ src/main/java/com/huskyhehe/eazyschool/validator/FieldsValueMatchValidator.java
@@ -18,11 +18,12 @@ public class FieldsValueMatchValidator implements ConstraintValidator<FieldsValu
     }
 
     @Override
-    public boolean isValid(Object value,ConstraintValidatorContext context) {
+    public boolean isValid(Object value, ConstraintValidatorContext context) {
         Object fieldValue = new BeanWrapperImpl(value)
                 .getPropertyValue(field);
         Object fieldMatchValue = new BeanWrapperImpl(value)
                 .getPropertyValue(fieldMatch);
+
         if (fieldValue != null) {
             return fieldValue.equals(fieldMatchValue);
         } else {
