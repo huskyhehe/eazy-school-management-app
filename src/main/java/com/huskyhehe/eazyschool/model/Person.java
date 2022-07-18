dca460bf44d6eb80953e344666cab002fb878262
huskyhehe <zhou.he1@northeastern.edu>
2022-07-17 17:53:57 -0700

    Update Person

--- src/main/java/com/huskyhehe/eazyschool/model/Person.java
+++ src/main/java/com/huskyhehe/eazyschool/model/Person.java
@@ -29,19 +29,19 @@ import javax.validation.constraints.Size;
 public class Person extends BaseEntity{
 
     @Id
-    @GeneratedValue(strategy= GenerationType.AUTO, generator="native")
-    @GenericGenerator(name = "native", strategy = "native")
+    @GeneratedValue(strategy = GenerationType.AUTO, generator="native")
+    @GenericGenerator(name = "native",strategy = "native")
     private int personId;
 
-    @NotBlank(message="Name must not be blank")
-    @Size(min=3, message="Name must be at least 3 characters long")
+    @NotBlank(message = "Name must not be blank")
+    @Size(min = 3, message = "Name must be at least 3 characters long")
     private String name;
 
-    @NotBlank(message="Mobile number must not be blank")
-    @Pattern(regexp="(^$|[0-9]{10})", message = "Mobile number must be 10 digits")
+    @NotBlank(message = "Mobile number must not be blank")
+    @Pattern(regexp = "(^$|[0-9]{10})",message = "Mobile number must be 10 digits")
     private String mobileNumber;
 
-    @NotBlank(message="Email must not be blank")
+    @NotBlank(message = "Email must not be blank")
     @Email(message = "Please provide a valid email address" )
     private String email;
 
@@ -50,21 +50,21 @@ public class Person extends BaseEntity{
     @Transient
     private String confirmEmail;
 
-    @NotBlank(message="Password must not be blank")
-    @Size(min=5, message="Password must be at least 5 characters long")
+    @NotBlank(message = "Password must not be blank")
+    @Size(min=5, message = "Password must be at least 5 characters long")
     @PasswordValidator
     private String pwd;
 
-    @NotBlank(message="Confirm Password must not be blank")
-    @Size(min=5, message="Confirm Password must be at least 5 characters long")
+    @NotBlank(message = "Confirm Password must not be blank")
+    @Size(min = 5, message = "Confirm Password must be at least 5 characters long")
     @Transient
     private String confirmPwd;
 
-    @OneToOne(fetch = FetchType.EAGER, cascade = CascadeType.PERSIST, targetEntity = Roles.class)
+    @OneToOne(fetch = FetchType.EAGER,cascade = CascadeType.PERSIST, targetEntity = Roles.class)
     @JoinColumn(name = "role_id", referencedColumnName = "roleId",nullable = false)
     private Roles roles;
 
-    @OneToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL, targetEntity = Address.class)
-    @JoinColumn(name = "address_id", referencedColumnName = "addressId", nullable = true)
+    @OneToOne(fetch = FetchType.EAGER,cascade = CascadeType.ALL, targetEntity = Address.class)
+    @JoinColumn(name = "address_id", referencedColumnName = "addressId",nullable = true)
     private Address address;
 }
