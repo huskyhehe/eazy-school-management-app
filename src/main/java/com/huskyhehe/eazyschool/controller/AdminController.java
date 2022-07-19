49eef8342a56d6484d9b67788c86996a79788550
huskyhehe <zhou.he1@northeastern.edu>
2022-07-19 15:03:46 -0700

    Implement sorting methods

--- src/main/java/com/huskyhehe/eazyschool/controller/AdminController.java
+++ src/main/java/com/huskyhehe/eazyschool/controller/AdminController.java
@@ -8,6 +8,7 @@ import com.huskyhehe.eazyschool.repository.EazyClassRepository;
 import com.huskyhehe.eazyschool.repository.PersonRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -116,7 +117,8 @@ public class AdminController {
 
     @GetMapping("/displayCourses")
     public ModelAndView displayCourses(Model model) {
-        List<Courses> courses = coursesRepository.findAll();
+        // List<Courses> courses = coursesRepository.findByOrderByName();
+        List<Courses> courses = coursesRepository.findAll(Sort.by("name").descending());
         ModelAndView modelAndView = new ModelAndView("courses_secure.html");
         modelAndView.addObject("courses",courses);
         modelAndView.addObject("course", new Courses());
