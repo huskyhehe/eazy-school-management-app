e535f1b1172ff378c876fa65841b04903cc80d50
huskyhehe <zhou.he1@northeastern.edu>
2022-07-20 16:45:34 -0700

    Add Response

---
+++ src/main/java/com/huskyhehe/eazyschool/model/Response.java
@@ -0,0 +1,15 @@
+package com.huskyhehe.eazyschool.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Response {
+
+    private String statusCode;
+    private String statusMsg;
+
+}
